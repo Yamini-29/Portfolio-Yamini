092af62a9fc77043b07055b647d7090aad19e8a1
yaminibaskar29 <cs21b056@iittp.ac.in>
2024-07-20 13:06:49 +0530

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
-
+import Hero from './components/Hero/Hero'
 function App() {
   return (
     <div className="App">
          <Navbar/>
+         <Hero/>
     </div>
   );
 }
